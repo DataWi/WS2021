6107dcb42d09b499a3f09dc95fd271a86fd95e2f
DataWi <wi21b094@technikum-wien.at>
2021-10-05 19:35:19 +0200

    minitest functions done

---
+++ tests/Functions.java
@@ -0,0 +1,11 @@
+package ws2021.tests;
+
+public class Functions {
+    public static void welcome(String name) {
+        System.out.printf("Welcome %s!", name);
+    }
+
+    public static int max(int number1, int number2){
+        return Math.max(number1, number2);
+    }
+}
